e989cf4259d7b0803ba243d0e31eab0e86d3125c
isaacgr <isaacgrowell@gmail.com>
2019-08-13 08:22:28 -0400

    Add levelorder traversal

--- binary_tree.py
+++ binary_tree.py
@@ -1,3 +1,27 @@
+class Queue(object):
+    def __init__(self):
+        self.items = []
+
+    def enqueue(self, item):
+        self.items.insert(0, item)
+
+    def dequeue(self):
+        if not self.is_empty():
+            return self.items.pop()
+
+    def is_empty(self):
+        return len(self.items) == 0
+
+    def peek(self):
+        if not self.is_empty():
+            return self.items[-1].value
+
+    def __len__ (self):
+        return self.size()
+
+    def size(self):
+        return len(self.items)
+
 class Node(object):
     def __init__(self, value):
         self.value = value
@@ -16,6 +40,8 @@ class BinaryTree(object):
             return self.inorder_print(tree.root, '')
         elif traversal_type == 'postorder':
             return self.postorder_print(tree.root, '')
+        elif traversal_type == 'levelorder':
+            return self.levelorder_print(tree.root)
         else:
             raise Exception('type not supported')
 
@@ -43,6 +69,24 @@ class BinaryTree(object):
             traversal += (str(start.value) + '-')
         return traversal
 
+    def levelorder_print(self, start):
+        if start is None:
+            return
+
+        queue = Queue()
+        queue.enqueue(start)
+        traversal = ""
+        while len(queue) > 0:
+            traversal += str(queue.peek()) + "-"
+            node = queue.dequeue()
+
+            if node.left:
+                queue.enqueue(node.left)
+            if node.right:
+                queue.enqueue(node.right)
+        return traversal
+
+
 tree = BinaryTree(1)
 tree.root.left = Node(2)
 tree.root.right = Node(3)
@@ -53,4 +97,5 @@ tree.root.right.right = Node(7)
 print tree.print_tree('preorder')
 print tree.print_tree('inorder')
 print tree.print_tree('postorder')
+print tree.print_tree('levelorder')
 
