1cc789f544126c916fa85f9703406c15f9cc22f1
isaacgr <isaacgrowell@gmail.com>
2019-08-14 07:54:13 -0400

    Add reverse levelorder method

--- binary_tree.py
+++ binary_tree.py
@@ -22,6 +22,30 @@ class Queue(object):
     def size(self):
         return len(self.items)
 
+class Stack(object):
+    def __init__(self):
+        self.items = []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        if not self.is_empty():
+            return self.items.pop()
+
+    def is_empty(self):
+        return len(self.items) == 0
+
+    def peek(self):
+        if not self.is_empty():
+            return self.items[-1]
+
+    def size(self):
+        return len(self.items)
+
+    def __len__(self):
+        return self.size()
+
 class Node(object):
     def __init__(self, value):
         self.value = value
@@ -42,6 +66,8 @@ class BinaryTree(object):
             return self.postorder_print(tree.root, '')
         elif traversal_type == 'levelorder':
             return self.levelorder_print(tree.root)
+        elif traversal_type == 'reverselevelorder':
+            return self.reverse_levelorder_print(tree.root)
         else:
             raise Exception('type not supported')
 
@@ -86,16 +112,37 @@ class BinaryTree(object):
                 queue.enqueue(node.right)
         return traversal
 
+    def reverse_levelorder_print(self, start):
+        if start is None:
+            return
+
+        queue = Queue()
+        stack = Stack()
+        queue.enqueue(start)
+
+        traversal = ''
+        while len(queue) > 0:
+            node = queue.dequeue()
+            stack.push(node)
+
+            if node.right:
+                queue.enqueue(node.right)
+            if node.left:
+                queue.enqueue(node.left)
+        while len(stack) > 0:
+            node = stack.pop()
+            traversal += str(node.value) + '-'
+        return traversal
 
 tree = BinaryTree(1)
 tree.root.left = Node(2)
 tree.root.right = Node(3)
 tree.root.left.left = Node(4)
 tree.root.left.right = Node(5)
-tree.root.right.left = Node(6)
-tree.root.right.right = Node(7)
+#tree.root.right.left = Node(6)
+#tree.root.right.right = Node(7)
 print tree.print_tree('preorder')
 print tree.print_tree('inorder')
 print tree.print_tree('postorder')
 print tree.print_tree('levelorder')
-
+print tree.print_tree('reverselevelorder')
